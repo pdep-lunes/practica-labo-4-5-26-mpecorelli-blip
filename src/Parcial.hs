ba9edb0f9315352ffd948cf9f09f5f5b4edd4029
Martina Pecorelli <mpecorelli@frba.utn.edu.ar>
2026-05-05 18:07:41 -0300

    Datas principales
    Declaro datas a usar que luego expandiré o modoficaré según sea necesario

--- src/Parcial.hs
+++ src/Parcial.hs
@@ -1,5 +1,22 @@
 module Parcial where
 import Text.Show.Functions()
 
-doble :: Int -> Int
-doble = (*2)
+//Hago la estructura por perrito
+data Perrito = unPerrito {
+  raza :: String,
+  juguetesFav :: [String],
+  tiempoEnGuarderia :: Int,
+  energia :: Int
+  }
+
+data Guarderia = unaGuarderia {
+   nombre :: String,
+   rutinaEntretenimiento :: [Actividad]
+   }
+
+data Actividad = unaActividad {
+   ejercicio :: Perrito -> Perrito,
+   tiempo :: Int
+   }
+//Ejercicio es una función que luego voy a desarrollar
+   
