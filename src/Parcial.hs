fd12bd84405b1a84660a25cd1be05e76badc13f6
Martina Pecorelli <mpecorelli@frba.utn.edu.ar>
2026-05-10 18:09:18 -0300

    Funciones modificacion energía

--- src/Parcial.hs
+++ src/Parcial.hs
@@ -7,16 +7,26 @@ data Perrito = unPerrito {
   juguetesFav :: [String],
   tiempoEnGuarderia :: Int,
   energia :: Int
-  }
+  } deriving (Show , Eq)
 
 data Guarderia = unaGuarderia {
    nombre :: String,
    rutinaEntretenimiento :: [Actividad]
-   }
+   } deriving (Show , Eq)
 
 data Actividad = unaActividad {
    ejercicio :: Perrito -> Perrito,
    tiempo :: Int
-   }
---Ejercicio es una función que luego voy a desarrollar*/
+   } deriving (Show , Eq)
+--Ejercicio es una función que luego voy a desarrollar
+
+--Modifica energía
+jugar: disminuye en 10 unidades la energía del perrito 🪫. ¡No puede quedar un valor negativo!
+ladrar: aumenta la energía la mitad de los ladridos que se establezcan. 
+
+jugar :: Perrito -> Perrito
+jugar unPerrito = unPerrito { energia = max 0 (energia unPerrito - 10) }
+
+ladrar :: Int -> Perrito -> Perrito
+ladrar CantLadridos unPerrito  = unPerrito { energia = (+ (cantLadridos `div` 2)) (energia unPerrito) }
    
